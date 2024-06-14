6c45337cd4a4fa7dfb90f158a19d6e99ddeb5bfd
karnowsky1 <kestime9015@bths.edu>
2024-06-14 03:18:10 -0400

    added GitGlobe component

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@/utils/cn";
 import { BackgroundGradientAnimation } from "./GradientBg";
+import { GlobeDemo } from "./GridGlobe";
 
 export const BentoGrid = ({
   className,
@@ -56,7 +57,7 @@ export const BentoGridItem = ({
             <img 
               src={img}
               alt={img}
-              className={cn(imgClassName, 'object-cover', 'object-center')}
+              className={cn(imgClassName, "object-cover object-center")}
             />
           )}
         </div>
@@ -65,26 +66,51 @@ export const BentoGridItem = ({
             <img 
               src={spareImg}
               alt={spareImg}
-              className={'object-cover, object-center, w-full h-full'}
+              className={'object-cover object-center w-full h-full'}
             />
           )}
         </div>
         {id === 6 && (
           <BackgroundGradientAnimation>
-            <div className="absolute z-50 flex items-center justify-center text-white font-bold"/>
+            <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl"/>
           </BackgroundGradientAnimation>
         )}
 
         <div className={cn(
-          titleClassName, 'group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10'
+          titleClassName, 
+          "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
         )}>
-          <div className="font-sans font-extralight text-[#c1c2d3] text-sm md:text-xs lg:text-base z">
+          <div className="font-sans font-extralight md:max-w-32 md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10">
             {description}
           </div>
-          <div className="font-sans font-bold text-lg lg:text-3xl max-w-96 z-10">
+          <div className="font-sans text-lg lg:text-3xl max-w-96 font-bold z-10">
             {title}
           </div>
         </div>
+        
+        {id === 2 && <GlobeDemo />}
+        {id === 3 && (
+          <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
+            <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
+              {['React.js, Next.js', 'TypeScript'].map
+              ((item) => (
+                <span key={item} className="py-2 lg:py4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">
+                  {item}
+                </span>
+              ))}
+              <span className="py-4 px-3 rounded-lg text-center bg-[#10132e]" />
+            </div>
+            <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
+              {['Swift', 'AWS', 'MongoDB'].map
+              ((item) => (
+                <span key={item} className="py-2 lg:py4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">
+                  {item}
+                </span>
+              ))}
+              <span className="py-4 px-3 rounded-lg text-center bg-[#10132e]" />
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
