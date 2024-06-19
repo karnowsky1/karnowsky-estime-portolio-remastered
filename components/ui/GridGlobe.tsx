bbdadd6f754b0882f10f302f6080f5a4c0a4272b
karnowsky1 <kestime9015@bths.edu>
2024-06-19 01:45:30 -0400

    UI bugs with git globe and recent projects gap

--- components/ui/GridGlobe.tsx
+++ components/ui/GridGlobe.tsx
@@ -395,7 +395,7 @@ export function GlobeDemo() {
   ];
 
   return (
-    <div className="flex item-center justify-center absolute -left-5 top-36 md:top-40 w-full h-full">
+    <div className="flex item-center justify-center absolute -left-5 top-12 sm:top-10 md:top-12 lg:top-32 xl:top-32 w-full h-full">
       <div className="max-w-7xl mx-auto w-full relative overflow-hidden px-4 h-96">
         <div className="absolute w-full bottom-0 inset-x-0 h-40 bg-gradient-to-b pointer-events-none select-none from-transparent dark:to-black to-white z-40" />
         <div className="absolute w-full h-72 md:h-full z-10">
