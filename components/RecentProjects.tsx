a14ac9d8310360e820cfbfc6c3e362a9cb2e516b
karnowsky1 <kestime9015@bths.edu>
2024-12-06 18:50:07 -0500

    recent projects UI change for mobile

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -53,8 +53,8 @@ const RecentProjects = () => {
                     </div>
                   ))}
                 </div>
-                  <div className="flex justify-center items-center">
-                    <p className="flex lg:text-xl md:text-xs text-sm text-purple">Check Live Site</p>
+                  <div className="flex justify-center items-center ">
+                    <p className="flex lg:text-xl md:text-xs sm:hidden text-sm text-purple">Check Live Site</p>
                     <FaLocationArrow className="ms-3" color="#CBACF9" />
                   </div>
                 </div>
